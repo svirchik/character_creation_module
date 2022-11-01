17c7cf2514a975b0b032346f8af79b46cbd3b344
Елена Васюченко <svirchik@yandex.ru>
2022-11-01 23:08:40 +0200

    Add screensaver

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
@@ -84,6 +86,7 @@ def choice_char_class() -> str:
 
 
 def main() -> None:
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -93,3 +96,6 @@ def main() -> None:
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
+
+
+main()
